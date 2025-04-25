716a13009e208495b80944a042ade1a23718fa5d
joaopaulomo <92831575+joaopaulomo@users.noreply.github.com>
2025-04-25 17:39:00 -0300

    Add files via upload

---
+++ max/run_mig.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Este arquivo é executado por um job.
+#
+# Os módulos devem ser recarregados e os ambiente reativados.
+
+module load anaconda3/2022.10
+module load openmpi/4.0.7-gcc-ucx-1.13.1
+
+source ~/.bashrc
+
+# É necessário desativar o ambiente atual primeiro devido a um bug.
+conda deactivate
+conda activate devito
+
+export DEVITO_LANGUAGE=openmp
+
+# Caminho absoluto do seu programa em python.
+PGR=/home/joao.santana/cluster_visco/code_rtm_implemnte/cluster_visco_max2.py
+
+python $PGR $WORK_DIR/$1 $WORK_DIR/$2
