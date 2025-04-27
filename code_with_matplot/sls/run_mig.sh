0cefdec80c7e3a20ab4f099474e93f35a23eea6f
joaopaulomo <92831575+joaopaulomo@users.noreply.github.com>
2025-04-27 20:53:37 -0300

    Add files via upload

---
+++ code_with_matplot/sls/run_mig.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Este arquivo é executado por um job.
+#
+# Os módulos devem ser recarregados e os ambiente reativados.
+
+module load anaconda3/2022.10
+module load openmpi/4.0.7-gcc-ucx-1.13.1
+
+source ~/.bashrc
+
+# É necessário desativar o ambiente atual primeiro devido a um bug.
+conda deactivate
+conda activate devito
+
+export DEVITO_LANGUAGE=openmp
+
+# Caminho absoluto do seu programa em python.
+PGR=/home/joao.santana/visco_cluster/code_rtm_implemnte/code_with_matplot/sls/cluster_visco_sls.py
+
+python $PGR $WORK_DIR/$1 $WORK_DIR/$2
