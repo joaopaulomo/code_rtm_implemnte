1b7c65f3031503e660d9c7c2819f0f774bc3b97a
joaopaulomo <92831575+joaopaulomo@users.noreply.github.com>
2025-05-09 18:38:12 -0300

    Add files via upload

---
+++ cluster_resampling.py
@@ -0,0 +1,119 @@
+from devito import *
+from examples.seismic import (
+    Model, AcquisitionGeometry, PointSource, RickerSource, TimeAxis
+)
+from examples.seismic.acoustic import AcousticWaveSolver
+from examples.seismic.plot_model_mod import (plot_image2,plot_shotrecord)
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
+
+import numpy as np
+import gc
+
+nx = 738
+nz = 240
+nbl = 85
+nshots = 738
+space_order = 8
+dtype = np.float32
+shape = (nx, nz)
+spacing = (12.5, 12.5)
+origin = (0., 0.)
+
+
+
+v = np.empty(shape, dtype=dtype)
+
+
+path = "marmousi-resample-738x240.bin"
+a = open(path)
+v = np.fromfile(a, dtype=dtype).reshape([nx, nz])
+v=v/1000
+model = Model(vp=v, origin=origin, shape=shape, spacing=spacing,
+              space_order=space_order, nbl=nbl, bcs="damp")
+
+model0 = Model(space_order=space_order, vp=v,   
+                           origin=origin, shape=shape, spacing=spacing, 
+                           nbl=nbl)
+filter_sigma = (6, 6)
+gaussian_smooth(model0.vp, sigma=filter_sigma)
+
+t0 = 0. 
+tn = 5000.
+dt = 0.0008
+time_range = TimeAxis(start = t0, stop = tn, step = dt)
+
+f0 = 0.030
+src = RickerSource(name='src', grid=model.grid, f0=f0, npoint=1, time_range=time_range)
+src.coordinates.data[0, :] = np.array(model.domain_size) * .5
+src.coordinates.data[0, -1] = 1.
+
+rec_coordinates = np.empty((model.shape[0], 2))
+rec_coordinates[:, 0] = np.linspace(0, model.domain_size[0], num=model.shape[0])
+rec_coordinates[:, 1] = 1.
+
+geometry = AcquisitionGeometry(model, rec_coordinates, src.coordinates.data, t0, tn, f0=f0, src_type='Ricker')
+
+solver = AcousticWaveSolver(model, geometry, space_order=space_order)
+true_d , _, _ = solver.forward(vp=model.vp)
+smooth_d, _, _ = solver.forward(vp=model0.vp)
+
+sismo_real = true_d.data
+sismo_smooth = smooth_d.data
+sismo_residual = smooth_d.data - true_d .data
+
+sismo_images = [sismo_real, sismo_smooth, sismo_residual]
+sismo_names = ["sismo_real.png", "sismo_smooth.png", "sismo_residual.png"]
+
+for imge, nam in zip(sismo_images, sismo_names):
+    plot_shotrecord(imge, model, t0, tn, clip_percent=99, clip_low=1)
+    plt.savefig(nam, dpi=300, bbox_inches='tight')
+    plt.close()
+
+def ImagingOperator(model, image):
+    v = TimeFunction(name='v', grid=model.grid, time_order=2, space_order=space_order)
+    u = TimeFunction(name='u', grid=model.grid, time_order=2, space_order=space_order, save=geometry.nt)
+    
+    eqn = model.m * v.dt2 - v.laplace + model.damp * v.dt.T
+    stencil = Eq(v.backward, solve(eqn, v.backward))
+    
+    dt = model.critical_dt
+    residual = PointSource(name='residual', grid=model.grid,
+                           time_range=geometry.time_axis,
+                           coordinates=geometry.rec_positions)    
+    res_term = residual.inject(field=v.backward, expr=residual * dt**2 / model.m)
+    image_update = Eq(image, image - u * v)
+
+    return Operator([stencil] + res_term + [image_update], subs=model.spacing_map)
+
+source_locations = np.empty((nshots, 2), dtype=np.float32)
+source_locations[:, 0] = np.linspace(0., model.domain_size[0], num=nshots)
+source_locations[:, 1] = 1
+
+image = Function(name='image', grid=model.grid,space_order=space_order)
+op_imaging = ImagingOperator(model, image)
+
+for i in range(nshots):
+    print('Imaging source %d out of %d' % (i+1, nshots))
+    geometry.src_positions[0, :] = source_locations[i, :]
+    true_d, _, _ = solver.forward(vp=model.vp)
+    smooth_d, u0, _ = solver.forward(vp=model0.vp, save=True)
+    v = TimeFunction(name='v', grid=model.grid, time_order=2, space_order=space_order)
+    residual = smooth_d.data - true_d.data
+    op_imaging(u=u0, v=v, vp=model0.vp, dt=model0.critical_dt, residual=residual)
+    gc.collect()
+    
+sliced_image = image.data[model.nbl:-model.nbl, model.nbl:-model.nbl]
+plot_image2(np.diff(sliced_image, axis=1),model,clip_percent=98,clip_low=2)
+plt.savefig('normal_variable.png', dpi=300, bbox_inches='tight')
+
+laplace_result = Function(name='lap',grid=model.grid, space_order=space_order)
+stencil = Eq(laplace_result, div(grad(image)))
+op = Operator([stencil])
+op.apply()
+
+sliced_laplace = laplace_result.data[model.nbl:-model.nbl, model.nbl:-model.nbl]
+plot_image2(sliced_laplace,model,clip_percent=98,clip_low=2)
+plt.savefig('lapla_variable.png', dpi=300, bbox_inches='tight')
+
